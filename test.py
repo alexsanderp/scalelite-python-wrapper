d79d5901ee594c45b5f8e400b6712c2c21bb4720
Alex Pereira <alexsander.pereira@icloud.com>
2020-06-27 17:48:04 -0300

    Remove test.py file

--- test.py
+++
@@ -1,9 +0,0 @@
-from scalelite import Scalelite
-
-scalelite = Scalelite(bin_path="docker exec scalelite-api bin/rake")
-
-servers = scalelite.list_servers()
-status = scalelite.status()
-
-print(servers)
-print(status)
