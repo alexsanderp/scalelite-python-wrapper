ecaebba847b5c9579697a8fcd4a53a18db58053f
Alex Pereira <alexsander.pereira@icloud.com>
2020-07-29 17:04:23 -0300

    Version 0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="scalelite",
-    version="0.1.3",
+    version="0.1.4",
     author='Alexsander Pereira',
     author_email='alexsander.pereira@icloud.com',
     description=u'A Python wrapper around the Scalelite',
